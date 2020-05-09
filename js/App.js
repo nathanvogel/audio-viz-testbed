b3a9a5f420a09231b97263ea79b2c7017e4449b0
Nathan Vogel <nathan@nathanvogel.com>
2020-05-09 03:38:38 +0200

    Can live switch between demos

--- js/App.js
+++ js/App.js
@@ -14,12 +14,23 @@ var App = function () {
   this.ctx = this.canvas.getContext("2d");
   this.tool = null;
   this.isMic = true;
+  this.demos = [];
+  this.currentDemo = 0;
   this.setup();
 };
 
 App.prototype = {
   setup: function () {
-    addEventListener("keydown", this.onKeyDown.bind(this));
+    this.demos = [
+      { demo: new Circles01Test(this.ctx, this.w, this.h), bandCount: 128 },
+      { demo: new TestRect01(this.ctx, this.w, this.h), bandCount: 128 },
+      { demo: new TestCircle02(this.ctx, this.w, this.h), bandCount: 128 },
+      { demo: new TestCircle03(this.ctx, this.w, this.h), bandCount: 32 },
+      { demo: new TestRect04(this.ctx, this.w, this.h), bandCount: 128 },
+      { demo: new TestRect05(this.ctx, this.w, this.h), bandCount: 128 },
+      { demo: new TestRect06(this.ctx, this.w, this.h), bandCount: 128 },
+      { demo: new TestRect07(this.ctx, this.w, this.h), bandCount: 128 },
+    ];
 
     // DEMOS
     // this.barDemo = new BarsDemo(this.ctx, this.w, this.h);
@@ -30,51 +41,50 @@ App.prototype = {
     // this.beatDemo = new BeatDemo(this.ctx, this.w, this.h);
     // this.verletDemo = new VerletDemo(this.canvas, this.w, this.h);
 
-    // this.circles01Test = new Circles01Test(this.ctx, this.w, this.h);
-    // this.testRect01 = new TestRect01(this.ctx, this.w, this.h);
-    // this.testCircle02 = new TestCircle02(this.ctx, this.w, this.h); // 128
-    this.testCircle03 = new TestCircle03(this.ctx, this.w, this.h); // 32
-    // this.testRect04 = new TestRect04(this.ctx, this.w, this.h); // 128
-    // this.testRect05 = new TestRect05(this.ctx, this.w, this.h); // 128
-    // this.testRect06 = new TestRect06(this.ctx, this.w, this.h); // 128
-    // this.testRect07 = new TestRect07(this.ctx, this.w, this.h); // 128
     // this.testImage01 = new TestImage01(this.ctx, this.w, this.h); // 128
-
     // this.maquette = new Maquette(this.canvas, this.w, this.h);
 
+    addEventListener("keydown", this.onKeyDown.bind(this));
     this.draw();
   },
 
   draw: function () {
     // clean canvas
-    // this.ctx.clearRect(0, 0, this.w, this.h); // ----> paperjs doesn't need
-    // draw stuff
+    this.ctx.clearRect(0, 0, this.w, this.h); // ----> comment for paperjs
     if (this.tool) {
       this.tool.updateFrequency();
       this.tool.updateWave();
       this.tool.analyzeBeats();
-
-      // this.barDemo.draw(this.tool.dataBeat);
-      // this.circleDemo.draw(this.tool.data);
-      // this.letterDemo.draw(this.tool.dataWave);
-      // this.rasterDemo.draw(this.tool.dataBeat);
-      // this.beatDemo.draw(this.tool.data, this.tool.dataWave);
-      // this.verletDemo.draw(this.tool.data);
-
-      // this.circles01Test.draw(this.tool.dataBeat);
-      // this.testRect01.draw(this.tool.dataBeat);
-      // this.testCircle02.draw(this.tool.dataBeat);
-      this.testCircle03.draw(this.tool.dataBeat);
-      // this.testRect04.draw(this.tool.dataBeat);
-      // this.testRect05.draw(this.tool.dataBeat);
-      // this.testRect06.draw(this.tool.dataBeat);
-      // this.testRect07.draw(this.tool.dataBeat);
-      // this.testImage01.draw(this.tool.dataBeat);
-
-      // this.maquette.draw(this.tool.dataBeat);
+      this.demos[this.currentDemo].demo.draw(this.tool.dataBeat);
     }
-    // refresh
     requestAnimationFrame(this.draw.bind(this));
+  },
+
+  setupMicAudioTool: function () {
+    const d = this.demos[this.currentDemo];
+    this.tool = new AudioTool("dummy-track-to-init-analyzernode");
+    this.tool.setupBeatDetector(d.bandCount, 1.25);
+    this.tool.update(null);
+    this.isMic = true;
+  },
+
+  nextDemo: function () {
+    this.currentDemo += 1;
+    if (this.currentDemo >= this.demos.length) this.currentDemo = 0;
+    this.setupDemo();
+  },
+
+  previousDemo: function () {
+    this.currentDemo -= 1;
+    if (this.currentDemo < 0) this.currentDemo = this.demos.length - 1;
+    this.setupDemo();
+  },
+
+  setupDemo: function () {
+    const d = this.demos[this.currentDemo];
+    // console.log(d);
+    // d.demo.setup();
+    this.tool.setupBeatDetector(d.bandCount, 1.25);
   },
 
   onKeyDown: function (e) {
@@ -82,12 +92,10 @@ App.prototype = {
 
     // Start the audio tool on the first keydown
     if (this.tool == null) {
-      this.tool = new AudioTool("dummy-track-to-init-analyzernode");
-      this.tool.setupBeatDetector(32, 1.25);
-      this.tool.update(null);
-      this.isMic = true;
+      this.setupMicAudioTool();
       return;
     }
+
     switch (e.keyCode) {
       case 32: // spacebar
         /*
@@ -116,6 +124,12 @@ App.prototype = {
         let audio = document.getElementsByTagName("audio")[0];
         audio.style.display =
           audio.style.display == "inline" ? "none" : "inline";
+        break;
+      case 70: // F
+        this.previousDemo();
+        break;
+      case 71: // G
+        this.nextDemo();
         break;
       case 37: // ArrowLeft
         this.tool.adjustThreshold(-0.1);
